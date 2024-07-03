82c7838a62c013cc24ff044db18955f8a8381f85
Heroshokca <139118221+Heroshokca@users.noreply.github.com>
2024-07-03 14:04:33 +0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -17,4 +17,35 @@ function submitForm() {
     document.getElementById("resultPosition").innerText = position;
     document.getElementById("resultOrganization").innerText = organization;
     document.getElementById("result").style.display = "block";
+
+    // Initialize Firebase
+    const firebaseConfig = {
+        apiKey: "AIzaSyCwU2HJ-W6cMUEfGjhydboxZIcg70Pxosc",
+        authDomain: "testlinebot-a6dec.firebaseapp.com",
+        databaseURL: "https://testlinebot-a6dec.firebaseio.com",
+        projectId: "testlinebot-a6dec",
+        storageBucket: "testlinebot-a6dec.appspot.com",
+        messagingSenderId: "504580392883",
+        appId: "1:504580392883:web:ad395aa890187aeb492dc6"
+    };
+
+    firebase.initializeApp(firebaseConfig);
+
+    // Get a reference to the database service
+    const database = firebase.database();
+
+    // Define a path and push the data
+    const blessingsRef = database.ref('signedBlessings');
+    blessingsRef.push({
+        blessing: blessing,
+        name: name,
+        position: position,
+        organization: organization
+    })
+    .then(() => {
+        console.log("Data successfully sent to Firebase!");
+    })
+    .catch((error) => {
+        console.error("Error sending data: ", error);
+    });
 }
