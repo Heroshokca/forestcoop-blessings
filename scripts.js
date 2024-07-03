36c3e3c3b496406a4cda7c5411b10ff22266e1c6
Heroshokca <139118221+Heroshokca@users.noreply.github.com>
2024-07-03 11:09:16 +0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,24 +1,21 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-app.js";
+import { getDatabase, ref, push, get } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-database.js";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCwU2HJ-W6cMUEfGjhydboxZIcg70Pxosc",
-  authDomain: "testlinebot-a6dec.firebaseapp.com",
-  databaseURL: "https://testlinebot-a6dec.firebaseio.com",
-  projectId: "testlinebot-a6dec",
-  storageBucket: "testlinebot-a6dec.appspot.com",
-  messagingSenderId: "504580392883",
-  appId: "1:504580392883:web:ad395aa890187aeb492dc6"
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+    databaseURL: "https://YOUR_PROJECT_ID.firebaseio.com",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_PROJECT_ID.appspot.com",
+    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+    appId: "YOUR_APP_ID"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-
-
+const database = getDatabase(app);
 
 function submitForm() {
     // Get form values
@@ -39,4 +36,35 @@ function submitForm() {
     document.getElementById("resultPosition").innerText = position;
     document.getElementById("resultOrganization").innerText = organization;
     document.getElementById("result").style.display = "block";
+
+    // Save data to Firebase
+    var newSignRef = ref(database, 'signatures');
+    push(newSignRef, {
+        blessing: blessing,
+        name: name,
+        position: position,
+        organization: organization
+    });
+}
+
+function exportCSV() {
+    const signaturesRef = ref(database, 'signatures');
+    get(signaturesRef).then((snapshot) => {
+        const data = snapshot.val();
+        let csvContent = "data:text/csv;charset=utf-8,คำถวายพระพร,ชื่อ,ตำแหน่ง,หน่วยงาน\n";
+
+        for (let id in data) {
+            const row = data[id];
+            csvContent += `${row.blessing},${row.name},${row.position},${row.organization}\n`;
+        }
+
+        const encodedUri = encodeURI(csvContent);
+        const link = document.createElement("a");
+        link.setAttribute("href", encodedUri);
+        link.setAttribute("download", "signatures.csv");
+        document.body.appendChild(link);
+
+        link.click();
+        document.body.removeChild(link);
+    });
 }
