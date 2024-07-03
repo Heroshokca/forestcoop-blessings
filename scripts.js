b4d26a781b95169ff442cd7fd206d298accc6e1d
Heroshokca <139118221+Heroshokca@users.noreply.github.com>
2024-07-03 10:30:06 +0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCwU2HJ-W6cMUEfGjhydboxZIcg70Pxosc",
+  authDomain: "testlinebot-a6dec.firebaseapp.com",
+  databaseURL: "https://testlinebot-a6dec.firebaseio.com",
+  projectId: "testlinebot-a6dec",
+  storageBucket: "testlinebot-a6dec.appspot.com",
+  messagingSenderId: "504580392883",
+  appId: "1:504580392883:web:ad395aa890187aeb492dc6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+
+
 function submitForm() {
     // Get form values
     var blessing = document.getElementById("blessing").value;
